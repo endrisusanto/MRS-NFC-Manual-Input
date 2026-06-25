5c3941427d74e3385644eae3369c7a3b71024ca2
endri Susanto <endrisusantosekarang@gmail.com>
2026-06-25 19:45:05 +0700

    Release v1.0.14

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mers-remote-v6';
+const CACHE = 'mers-remote-v7';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon.svg'];
 
 self.addEventListener('install', event => {
