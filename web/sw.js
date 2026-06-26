cb64f393abad5505be20e45bd05c810705013c6c
endri Susanto <endrisusantosekarang@gmail.com>
2026-06-26 17:41:41 +0700

    fix: split order view to PWA web carousel and Android modal and bump sw to v11

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mers-remote-v10';
+const CACHE = 'mers-remote-v11';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'mers-logo.png', 'mers-gen-map.js'];
 
 self.addEventListener('install', event => {
